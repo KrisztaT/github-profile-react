23f74328428986555ec70cf1401c643d7bf37634
KrisztaT <itsforadsmail@gmail.com>
2023-04-30 18:06:56 +1000

    comments and format updated

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,29 +5,29 @@ import React from "react";
  * the user's profile information and a list of their repositories.
  */
 const Profile = ({ user, repos }) => {
-    return (
-      <div className="container">
-        {user && (
-            <div className="inner-container">
-                <img className="avatar" src={user.avatar_url} alt={user.login} />
-                <div className='info-card'>
-                    <h1>{user.name}</h1>
-                    <p>{user.bio}</p>
-                    <div className="stats">
-                        <p> {user.followers} Followers </p>
-                        <p> {user.following} Following </p>
-                        <p> {user.public_repos} Repos </p>
-                    </div>
-                </div>
-            </div> 
-        )}
-        <div className="repos">
-            {repos.map((repo) => (
-                <a href={repo.html_url}>{repo.name}, </a>
-            ))}
+  return (
+    <div className="container">
+      {user && (
+        <div className="inner-container">
+          <img className="avatar" src={user.avatar_url} alt={user.login} />
+          <div className="info-card">
+            <h1>{user.name}</h1>
+            <p>{user.bio}</p>
+            <div className="stats">
+              <p> {user.followers} Followers </p>
+              <p> {user.following} Following </p>
+              <p> {user.public_repos} Repos </p>
+            </div>
           </div>
         </div>
-    );
-  };
-  
-  export default Profile;
+      )}
+      <div className="repos">
+        {repos.map((repo) => (
+          <a href={repo.html_url}>{repo.name}, </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
